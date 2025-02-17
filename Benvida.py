b2bd5771dc5fd54a3067efa4201b09a2c9a40a4a
luciadixi <luciadixitalizacion@gmail.com>
2025-02-17 16:50:10 +0100

    Add files via upload

---
+++ Benvida.py
@@ -0,0 +1,13 @@
+idade = input ("cantos anos tes ")
+print ("Que madurito estas , idade , anos que mogollón ")
+pyt
+")
+
+print("Boas tardes
+idade = input ("cantos años tes ")
+print ("Feliz día")
+print ("ahora matematicas")
+numerito = input
+numerito = int(input( "Introduce un número")
+print ("0 dobre do teu número é")
+")
