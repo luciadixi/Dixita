9c5a810ebee3fcd48d5d19aae1988c66fe16532f
luciadixi <luciadixitalizacion@gmail.com>
2025-02-18 13:45:44 +0100

    Add files via upload

---
+++ Tarefa1.py
@@ -0,0 +1,16 @@
+>>> print("Moi boas a todos")
+Moi boas a todos
+>>> print("2+2")
+2+2
+>>> print(2+2)
+4
+>>> print(4*5)
+20
+>>> print(6*9)
+54
+>>> print("8+8")
+8+8
+>>> print("lucía")
+lucía
+>>> print(36-45)
+-9
