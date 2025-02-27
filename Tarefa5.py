b578bbb258502877180164adae06895c7d867998
luciadixi <luciadixitalizacion@gmail.com>
2025-02-27 13:42:13 +0100

    Update Tarefa5.py

---
+++ Tarefa5.py
@@ -0,0 +1,6 @@
+contestado = input("Cal é o teu número favorito? ")
+numerito = int(contestado)
+triple = numerito*3
+print("Tres veces do teu número favorito é.... ", triple)
+contestado = input("cantas ducias de ovos leva a súa tortilla?")
+ovos = int(contestado)
