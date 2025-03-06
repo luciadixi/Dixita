bc048aeccae91cc7d95dd09952ec3c5f7ed77481
luciadixi <luciadixitalizacion@gmail.com>
2025-03-06 13:58:27 +0100

    Add files via upload

---
+++ Tarefa6_condicionais.py
@@ -0,0 +1,19 @@
+persoa = "profe"
+if(persoa == "profe"):
+    print("bos días  ",persoa)
+elif (persoa == "Marcos"): 
+elif (persoa == "Lucía"):
+elif (persoa == "Pita"):
+    print("É unha persoa moi maja")
+    print("Esta diante da ventá")
+    print("Traballa un chisco")
+else:
+    print("Non sei quen es")
+persoa = input ("Como te chamas?  ")
+
+
+persoa = ("Cal é o teu número favorito?")
+if(persoa == "Cal é o teu número favorito?"):
+    print("O meu número favorito é o 6"  ,profesora)
+    else:
+
