d325efe48a178f3a25f0054ef871c69580734c76
luciadixi <luciadixitalizacion@gmail.com>
2025-03-10 16:27:49 +0100

    Update Tarefa5_ducias2ovos.py

--- Tarefa5_ducias2ovos.py
+++ Tarefa5_ducias2ovos.py
@@ -1,6 +1,6 @@
 contestado = input("Cal é o teu número favorito? ")
-numerito = int(contestado)
-triple = numerito*3
+numeriño = int(contestado)
+triple = numeriño*3
 print("Tres veces do teu número favorito é.... ", triple)
 contestado = input("cantas ducias de ovos leva a súa tortilla?")
 ovos = int(contestado)
