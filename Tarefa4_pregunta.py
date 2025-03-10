5ca44a97119dc37baf8139076dc4f679ddcede8a
luciadixi <luciadixitalizacion@gmail.com>
2025-03-10 16:30:32 +0100

    Update Tarefa4_pregunta.py

--- Tarefa4_pregunta.py
+++ Tarefa4_pregunta.py
@@ -1,6 +1,6 @@
-resp = input("Como é o teu nome?")
-print("Que nome máis bonito")
-print("Ti es unha amable persoa")
-print("Vales moitísimo")
-print("Non hay ninguén máis lista ca", resp)
-print("e moi bo,resp,o mundo adórate")
+resp = input("Como é o teu nome?");
+print("Que nome máis bonito" , resp);
+print("Ti es unha amable persoa " , resp);
+print("Vales moitísimo " , resp);
+print("Non hay ninguén máis lista ca", resp);
+print("e moi bo" , resp ,o mundo adórate!");
