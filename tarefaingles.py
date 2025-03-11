c026b6e09a8fcf7b2dee6ffafeeff769ab38c593
luciadixi <luciadixitalizacion@gmail.com>
2025-03-11 13:58:32 +0100

    Add files via upload

---
+++ tarefaingles.py
@@ -0,0 +1,13 @@
+neno = "profe"
+if(neno == "profe")
+print("como se di casa en ingles ", neno)
+print("como se di agua en ingles ", neno)
+elif (neno == "house")
+elif (neno == "water")
+print("¡Moi ben! ", persoa)
+else:
+    print("como se di gato en ingles ", persoa)
+    print("se di cat ", neno)
+    print("como se di zapatos en ingles, persoa")
+    input("se di shoes ",neno)
+    
