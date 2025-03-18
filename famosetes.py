9505ea04c481b9177eefdf10be74b045c6b6b7be
luciadixi <luciadixitalizacion@gmail.com>
2025-03-18 13:57:51 +0100

    Add files via upload

---
+++ famosetes.py
@@ -0,0 +1,22 @@
+print("benvidos a deportes e famosos")
+deporte =input("Que deporte practica?")
+if (deporte == "boxeo")
+país =input("Cal é o seu país?")
+if (país == "estados unidos"):
+    print("O teu famoso deportista é Stalone")
+
+print("benvidos a deportes e famosos")
+atletismo =input("Que deporte practica?")
+if ("deporte == atletismo")
+print("A túa deportista é Ana Peleteiro")
+
+print("benvidos a deportes e famosos")
+badminton =input("Que deporte practica?")
+if ("deporte == badminton")
+print("A túa deportista é Carolina Marín")
+
+atletismo =input("Que deporte practica?")
+ollos =input ("Cal e o seu cor de ollos?")
+if (ollos == marróns)
+if ("deporte == atletismo")
+print("O teu deportista é Fermín Cacho")
