412d12ede5bb028dbaf33c2848106011f467e628
luciadixi <luciadixitalizacion@gmail.com>
2025-03-20 13:53:42 +0100

    Add files via upload

---
+++ caixarapida.py
@@ -0,0 +1,21 @@
+# Engade arriba a primeira liña de todos os .py
+
+print("benvidos ao supermercado")
+
+
+# Nome de ficheiro: caixarapida.py
+# Autor: recepcionista da caixa
+
+
+print("benvido ao noso supermercado!, persoa")
+
+else: 
+    print("canto costa o primeiro producto?")
+input("o prezo é de 5 euros")
+    print("canto costa o segundo producto?")
+input("o prezo é de 10 euros")
+    print("cal é o prezo do terceiro producto?")
+input("o prezo son 20 euros")
+
+
+# Mostra na pantalla a suma dos tres prezos, en bonito, como se fose o ticket de compra
